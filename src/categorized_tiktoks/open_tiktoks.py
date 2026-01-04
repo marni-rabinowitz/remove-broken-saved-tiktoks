02e33becbe9b62bd9dd2b25b3cd5300bccff4508
Marni Rabinowitz <marnir629@gmail.com>
2026-01-04 13:54:35 -0600

    went thru most of pets

--- src/categorized_tiktoks/open_tiktoks.py
+++ src/categorized_tiktoks/open_tiktoks.py
@@ -2,7 +2,7 @@ import webbrowser
 
 # --- CONFIGURATION ---
 BATCH_SIZE = 10
-LINKS_FILE = "pets_formatted.txt"
+LINKS_FILE = "cooking_formatted.txt"
 # ----------------------
 
 def open_links_in_batches(file_path):
