5cadae7ddb5c3b3c867455f5f50e4b4274bb7a09
Marni Rabinowitz <marnir629@gmail.com>
2026-01-13 18:08:54 -0600

    went thru more

--- src/categorized_tiktoks/open_tiktoks.py
+++ src/categorized_tiktoks/open_tiktoks.py
@@ -2,7 +2,7 @@ import webbrowser
 
 # --- CONFIGURATION ---
 BATCH_SIZE = 10
-LINKS_FILE = "business_and_career_formatted.txt"
+LINKS_FILE = "gaming_and_esports_formatted.txt"
 # ----------------------
 
 def open_links_in_batches(file_path):
