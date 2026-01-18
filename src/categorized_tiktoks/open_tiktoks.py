f2ea5edb9a622dff21e7509ad02a4d4c271f7e47
Marni Rabinowitz <marnir629@gmail.com>
2026-01-17 20:42:53 -0600

    went thru more

--- src/categorized_tiktoks/open_tiktoks.py
+++ src/categorized_tiktoks/open_tiktoks.py
@@ -4,7 +4,7 @@ import random
 
 # --- CONFIGURATION ---
 BATCH_SIZE = 10
-LINKS_FILE = "home_and_interior_formatted.txt"
+LINKS_FILE = "comedy_and_entertainment_formatted.txt"
 
 # Human-like delay settings (seconds)
 MIN_DELAY = .1
